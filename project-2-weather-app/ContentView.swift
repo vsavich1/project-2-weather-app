fd44224aa048b7bd0da78d8cc370d7464dadebac
Valentyn Savich <vsavich@unc.edu>
2026-03-17 14:18:42 -0400

    Initial Commit

---
+++ project-2-weather-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project-2-weather-app
+//
+//  Created by Valentyn Savich on 3/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project-2-weather-app/project_2_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_2_weather_appApp.swift
+//  project-2-weather-app
+//
+//  Created by Valentyn Savich on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct project_2_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
